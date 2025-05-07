93223b7d34868caa47cf792e949831c195385483
Elliot Berman <eberman@anduril.com>
2025-05-07 13:12:21 -0700

    DO NOT MERGE: Disable multimedia and vpi tests
    Will revisit with the 25.05 release.

--- pkgs/samples/combined-test.nix
+++ pkgs/samples/combined-test.nix
@@ -2,8 +2,8 @@
 , cudnn-test
 , cupti-test
 , libnvinfer-test
-, multimedia-test
-, vpi2-test
+  # , multimedia-test
+  # , vpi2-test
 , writeShellApplication
 }:
 writeShellApplication {
@@ -28,15 +28,5 @@ writeShellApplication {
     echo "Running TensorRT test"
     echo "====="
     ${libnvinfer-test}/bin/libnvinfer-test
-
-    echo "====="
-    echo "Running Multimedia test"
-    echo "====="
-    ${multimedia-test}/bin/multimedia-test
-
-    echo "====="
-    echo "Running VPI2 test"
-    echo "====="
-    ${vpi2-test}/bin/vpi2-test
   '';
 }
